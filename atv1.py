72ea7e3f639991e3d299eab90b178113179a2edd
joaovvmorais <144918314+joaovvmorais@users.noreply.github.com>
2023-09-20 12:05:24 -0300

    Add files via upload

---
+++ atv1.py
@@ -0,0 +1,17 @@
+print('Hello World!')
+
+num1=float(input('Escreva um número:'))
+num2=float(input('Escreva um número:'))
+
+soma = num1 + num2
+subtração = num1 - num2
+multiplicação = num1 * num2
+divisão = num1 / num2
+
+print(f'Soma: {soma}')
+print(f'Subtração: {subtração}')
+print(f'Multiplicação: {multiplicação}')
+print(f'Divisão: {divisão}')
+
+nome = input('Insira seu nome: ')
+print(f'Bem vindo {nome}!')
